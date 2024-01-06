5aa30e50c35eb5cf7611b5266f515d2739b79324
naim195 <naimkhan.shaikh@iitgn.ac.in>
2024-01-06 23:00:03 +0530

    used class

--- script.js
+++ script.js
@@ -1,63 +1,74 @@
-let myLibrary = [];
-let bookId=0;
-
-function Book(name,author,status) {
-  // the constructor...
-  this.name = name;
-  this.author = author;
-  this.status = status;
-  
-}
-
-function addBookToLibrary(name, author, status) {
-  // do stuff here
-    const newBook=new Book(name,author,status);
-    myLibrary.push(newBook);
-    bookId++;
-}
-
-let inpBook=document.querySelector('#name');
-let inpAuthor=document.querySelector('#author');
-let ReadUnread=document.querySelector('#status');
-let form=document.querySelector('#book-form');
-let table=document.querySelector('.table');
-let properties=['name','author', 'status']
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    addBookToLibrary(inpBook.value, inpAuthor.value, ReadUnread.value);
-    let tr = document.createElement('tr');
-    let btn1 = document.createElement('button');
-    btn1.className = 'button-status';
-    let btn2 = document.createElement('button');
-    btn2.className = 'button-del';
-    btn2.innerText = 'Delete';
-    tr.id='row-'+bookId;
-    document.getElementById('book-table-body').appendChild(tr); // Append to tbody
-
-    for (let i = 0; i < 2; i++) {
-        let td = document.createElement('td');
-        td.innerText = myLibrary[myLibrary.length - 1][properties[i]];
-        tr.appendChild(td);
+class Book {
+    constructor(name, author, status) {
+      this.name = name;
+      this.author = author;
+      this.status = status;
     }
-    btn1.innerText = ReadUnread.value;
-    tr.appendChild(btn1);
-    tr.appendChild(btn2);
+  }
+
+  class Library {
+    constructor() {
+      this.myLibrary = [];
+      this.bookId = 0;
+    }
+
+    addBookToLibrary(name, author, status) {
+      const newBook = new Book(name, author, status);
+      this.myLibrary.push(newBook);
+      this.bookId++;
+      return newBook;
+    }
+
+    removeBookFromLibrary(id) {
+      this.myLibrary.splice(id, 1);
+    }
+  }
+
+  const library = new Library();
+
+  const form = document.querySelector('#book-form');
+  const tableBody = document.getElementById('book-table-body');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+    const inpBook = document.querySelector('#name');
+    const inpAuthor = document.querySelector('#author');
+    const readUnread = document.querySelector('#status');
+
+    const newBook = library.addBookToLibrary(inpBook.value, inpAuthor.value, readUnread.value);
     
+    const tr = document.createElement('tr');
+    tr.id = 'row-' + library.bookId;
+
+    for (const property of ['name', 'author', 'status']) {
+      const td = document.createElement('td');
+      td.innerText = newBook[property];
+      tr.appendChild(td);
+    }
+
+    const statusBtn = document.createElement('button');
+    statusBtn.className = 'button-status';
+    statusBtn.innerText = readUnread.value;
+    tr.appendChild(statusBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'button-del';
+    deleteBtn.innerText = 'Delete';
+    tr.appendChild(deleteBtn);
+
+    tableBody.appendChild(tr);
+
     inpBook.value = '';
     inpAuthor.value = '';
 
-    btn1.addEventListener('click', () => {
-        btn1.innerText = btn1.innerText === 'Read' ? 'Unread' : 'Read';
+    statusBtn.addEventListener('click', () => {
+      statusBtn.innerText = statusBtn.innerText === 'Read' ? 'Unread' : 'Read';
     });
 
-    btn2.addEventListener('click',()=>{
-        let delId=tr.id.substring(3);
-        myLibrary.splice(delId,1);
-        tr.remove();
-        
+    deleteBtn.addEventListener('click', () => {
+      const delId = tr.id.substring(3);
+      library.removeBookFromLibrary(delId);
+      tr.remove();
     });
-});
-
-
+  });
